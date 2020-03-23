c54a1e3476adbd7accf890109082272929b7bd64
YaroslavAnt <antonchyk.yar@gmail.com>
2020-03-23 15:39:32 +0200

    taskbox UI

---
+++ src/components/InboxScreen.stories.js
@@ -0,0 +1,26 @@
+﻿import React from "react";
+import { action } from "@storybook/addon-actions";
+import { Provider } from "react-redux";
+import { defaultTasksData } from "./TaskList.stories";
+import { PureInboxScreen } from "./InboxScreen";
+
+export default {
+  component: PureInboxScreen,
+  title: "InboxScreen",
+  decorators: [story => <Provider store={store}>{story()}</Provider>]
+};
+
+// A simple mock of a redux store
+const store = {
+  getState: () => {
+    return {
+      tasks: defaultTasksData
+    };
+  },
+  subscribe: () => 0,
+  dispatch: action("dispatch")
+};
+
+export const Default = () => <PureInboxScreen />;
+
+export const Error = () => <PureInboxScreen error="Something" />;
